c7eb104298759711b1de77358e0a6384db8f5d35
jm-wltr <90411864+jm-wltr@users.noreply.github.com>
2021-10-07 18:57:47 +0200

    Working version

--- maclaurin.py
+++ maclaurin.py
@@ -11,15 +11,17 @@ import matplotlib.pyplot as plt
 import decimal
 from math import factorial
 
-maclaurin1 = np.linspace(0,2*np.pi, 25)
-maclaurin2 = maclaurin1
+maclaurin1 = np.linspace(-4*np.pi,4*np.pi, 500)
+maclaurin2 = np.linspace(-4*np.pi,4*np.pi, 500)
     
-for n in range(1):
+for n in range(50):
     if n != 0:
-        maclaurin2 += (-1)**n * maclaurin1**(2*n+1) / factorial(2*n+1)
+        maclaurin2 = maclaurin2 + ((-1)**n * maclaurin1**(2*n+1) / factorial(2*n+1))
     print(maclaurin2)
+    
     print()
     plt.plot(maclaurin1, maclaurin2)
 plt.plot(maclaurin1, np.sin(maclaurin1))
-    
+
+plt.gca().set_ylim(-4, 4)
 
